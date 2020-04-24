2640c0bed31267fb0e3a1c2838a7f8a051dda637
Phamquocduy1110@gmail.com <Sakuraandusagi*123>
2020-04-24 19:30:06 +0700

    Create Class Library (.NET FrameworK) and create Unit Test Project (.NET Framework) then create UnitTest_Exercise2.Test in Unit Test Project

---
+++ UnitTest_Exercise2.Test/UnitTest_Exercise2.Test/AverageTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest_Exercise2.Test
+{
+    [TestClass]
+    public class AverageTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ UnitTest_Exercise2.Test/UnitTest_Exercise2.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest_Exercise2.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest_Exercise2.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0905fbb3-a660-4f35-9eb2-6b12187a264c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
